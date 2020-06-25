67a85795cb840ca8539ff0007e6b16ed9c8ae270
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2020-06-25 17:50:08 +0200

    ok

--- js/aziendaagricola.js
+++ js/aziendaagricola.js
@@ -21,13 +21,38 @@ var timeoutHandle2 = window.setTimeout(function(){
 //   }, 2000);}
 // });
 
-$(window).scroll(function(){
-var scroll = $(window).scrollTop();
-if (scroll >= 1900) {
-  setTimeout(timeoutHandle);
-setTimeout(timeoutHandle2);}
-if (scroll < 1900){
-  window.clearTimeout(timeoutHandle);
-    window.clearTimeout(timeoutHandle2);
+// $(window).scroll(function(){
+// var scroll = $(window).scrollTop();
+// if (scroll > 3000) {
+//   setTimeout(timeoutHandle);
+// setTimeout(timeoutHandle2);}
+// if (scroll < 1900){
+//   window.clearTimeout(timeoutHandle);
+//     window.clearTimeout(timeoutHandle2);
+// }
+// });
+
+
+$(allInView);
+$(window).scroll(allInView);
+
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
-});
+
+function allInView() {
+
+//     if (isScrolledIntoView($("#numbersSection")){
+//       setTimeout(timeoutHandle);
+//   setTimeout(timeoutHandle2);
+// }
+
+ if (isScrolledIntoView($("#numbersSection"))) setTimeout(timeoutHandle); setTimeout(timeoutHandle2);
+  }
