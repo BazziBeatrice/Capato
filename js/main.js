e5c7df87e47d8559418dc2880f7ecacc80a96847
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2020-06-30 17:49:13 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
                 $("#MAKE").hover(
                         function(){
                                         $("#CategoryContainer").addClass("CategoryImgFadeIn");
-                                        $("#CategoryContainer").css("background-image", "url('assets/images/categorie/azienda3.jpg')")
+                                        $("#CategoryContainer").css("background-image", "url('assets/images/categorie/cat1-black.jpg')")
                                   },
 
                         function(){     $("#CategoryContainer").removeClass("CategoryImgFadeIn");
@@ -53,7 +53,7 @@ $(document).ready(function(){
                  $("#LEAVE").hover(
                         function(){
                                         $("#CategoryContainer").addClass("CategoryImgFadeIn");
-                                        $("#CategoryContainer").css("background-image", "url('assets/images/categorie/coltivazioni.jpg')")
+                                        $("#CategoryContainer").css("background-image", "url('assets/images/categorie/cat2-black.jpg')")
                                   },
 
                         function(){
@@ -65,7 +65,7 @@ $(document).ready(function(){
                  $("#SWIM").hover(
                         function(){
                                         $("#CategoryContainer").addClass("CategoryImgFadeIn");
-                                        $("#CategoryContainer").css("background-image", "url('assets/images/categorie/trasporto.jpg')")
+                                        $("#CategoryContainer").css("background-image", "url('assets/images/categorie/cat3-black.jpg')")
                                   },
 
                         function(){
@@ -583,3 +583,44 @@ span.onclick = function() {
 //hover modal coltivazioni----------------------------
 
 // sessionStorage.setItem('body', 'Tom');
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
