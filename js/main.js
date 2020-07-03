89c19c48b6d4eefca1b657606b22b0b445699955
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2020-07-03 15:36:01 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -25,9 +25,24 @@ var language;
 $(document).ready(function(){
 
   if ($(window).width() < 768) {
+    document.getElementById("step1-video").src = "../assets/images/produzione/baulaturaterreno.mp4";
+    document.getElementById("step1-video").load();
+    document.getElementById("step2-video").src = "../assets/images/produzione/trapianto-mobile.mp4";
+    document.getElementById("step2-video").load();
+    document.getElementById("step3-video").src = "../assets/images/produzione/sarchiatura.mp4";
+    document.getElementById("step3-video").load();
+    document.getElementById("step4-video").src = "../assets/images/produzione/raccolta.mp4";
+    document.getElementById("step4-video").load();
+    document.getElementById("step5-video").src = "../assets/images/produzione/lavaggio.mp4";
+    document.getElementById("step5-video").load();
     document.getElementById("step6-video").src = "../assets/images/produzione/stoccaggio.mp4";
-document.getElementById("step6-video").load();
+    document.getElementById("step6-video").load();
+    document.getElementById("step7-video").src = "../assets/images/produzione/trasporto2.mp4";
+    document.getElementById("step7-video").load();
+    document.getElementById("step6-video").src = "../assets/images/produzione/stoccaggio.mp4";
+    document.getElementById("step6-video").load();
 }
+
 // CheckLanguage();
 // function CheckLanguage(){
 // };
